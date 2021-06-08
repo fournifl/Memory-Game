0cfa5d9f4d270a12a2c5ab1d405552c936d01ba5
Florent Fournier <florent.fournier@protonmail.com>
2021-06-08 22:34:36 +0200

    updating game config for window size, sound and image data

--- game_config.py
+++ game_config.py
@@ -1,11 +1,14 @@
 import os
 
-IMAGE_SIZE = 128
-SCREEN_SIZE = 512
+IMAGE_SIZE = 220
+SCREEN_SIZE = 220 * 4
 NUM_TILES_SIDE = 4
 NUM_TILES_TOTAL = 16
 MARGIN = 8
 
 ASSET_DIR = 'assets'
-ASSET_FILES = [x for x in os.listdir(ASSET_DIR) if x[-3:].lower() == 'png']
+SOUND_DIR = 'sounds'
+# ~ ASSET_FILES = [x for x in os.listdir(ASSET_DIR) if x[-3:].lower() == 'png']
+ASSET_FILES = [x for x in os.listdir(ASSET_DIR) if x[-4:].lower() == 'jpeg']
+print(ASSET_FILES)
 assert len(ASSET_FILES) == 8
