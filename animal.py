3ae706b1270857a55ceceb680c6b65c749cb6b6b
Florent Fournier <florent.fournier@protonmail.com>
2023-08-15 19:30:28 +0200

    creation of get_row_of_index and get_col_of_index

--- animal.py
+++ animal.py
@@ -1,3 +1,4 @@
+import pdb
 import random
 import os
 import game_config as gc
@@ -6,8 +7,21 @@ from pygame import image, transform
 
 animals_count = dict((a, 0) for a in gc.ASSET_FILES)
 
+
 def available_animals():
     return [animal for animal, count in animals_count.items() if count < 2]
+
+
+def get_row_of_index(index):
+    return index // gc.NUM_TILES_WIDTH
+
+
+def get_col_of_index(index):
+    return index % gc.NUM_TILES_WIDTH
+
+
+def test(index):
+    return index // gc.NUM_TILES_WIDTH, index % gc.NUM_TILES_HEIGHT
 
 class Animal:
     def __init__(self, index):
@@ -15,8 +29,8 @@ class Animal:
         self.name = random.choice(available_animals())
         self.image_path = os.path.join(gc.ASSET_DIR, self.name)
         self.sound_path = os.path.join(gc.SOUND_DIR, self.name.replace('.jpeg', '.wav'))
-        self.row = index // gc.NUM_TILES_SIDE
-        self.col = index % gc.NUM_TILES_SIDE
+        self.row = get_row_of_index(index)
+        self.col = get_col_of_index(index)
         self.skip = False
         self.image = image.load(self.image_path)
         self.image = transform.scale(self.image, (gc.IMAGE_SIZE - 2 * gc.MARGIN, gc.IMAGE_SIZE - 2 * gc.MARGIN))
